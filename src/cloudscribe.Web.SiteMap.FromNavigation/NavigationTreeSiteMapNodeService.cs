8833da895be8becbecefa657d0200df77a9a6e0a
Joe Audette <joeaudette@users.noreply.github.com>
2016-04-21 14:53:39 -0400

    minor tidy

--- src/cloudscribe.Web.SiteMap.FromNavigation/NavigationTreeSiteMapNodeService.cs
+++ src/cloudscribe.Web.SiteMap.FromNavigation/NavigationTreeSiteMapNodeService.cs
@@ -5,15 +5,13 @@
 // Last Modified:           2016-04-20
 // 
 
-using Microsoft.AspNet.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using cloudscribe.Web.Navigation;
 using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace cloudscribe.Web.SiteMap
 {
